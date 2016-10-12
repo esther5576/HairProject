f56e139179764cb63a68e666e693283aced1dcb3
esther5576 <esther.berges@hotmail.com>
2016-10-12 18:05:11 +0200

    ProjetUnity

---
+++ Projet_Unity/Assets/Esther/C#/MouseController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour 
+{
+
+	public GameObject _lacets;
+	public GameObject _pointer;
+
+	GameObject _lacetsInstantiate;
+	GameObject _pointerInstantiate;
+
+	Vector3 offset;
+	Vector3 screenPoint;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			_lacetsInstantiate = GameObject.Instantiate (_lacets, Input.mousePosition, Quaternion.identity) as GameObject;
+			_pointerInstantiate = GameObject.Instantiate (_pointer, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+			_lacetsInstantiate.GetComponent<Kvant.WigController> ().target = _pointerInstantiate.transform;
+		}
+		if (Input.GetMouseButton (0)) 
+		{
+			
+		}
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			_lacetsInstantiate = null;
+			_pointerInstantiate = null;
+		}
+	}
+}
