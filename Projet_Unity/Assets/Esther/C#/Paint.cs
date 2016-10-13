1a14151b95d881cdfdc1ba3d9ed9b350715734cf
Ben <benjamin.cornaglia@gmail.com>
2016-10-13 17:19:41 +0200

    [CODE] V0.1 du paint

---
+++ Projet_Unity/Assets/Esther/C#/Paint.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Paint : MonoBehaviour {
+
+    
+    List<Vector3> m_avLinePoints = new List<Vector3>();
+    
+    LineRenderer m_pLineRenderer;
+    public float m_fStartWidth = 1.0f;
+    public float m_fEndWidth = 1.0f;
+    public float m_fThreshold = 0.001f;
+    Camera m_pCamera;
+    int m_iLineCount = 0;
+
+    Vector3 m_vLastPos = Vector3.one * float.MaxValue;
+
+    public GameObject m_pDrawer;
+    
+
+    //public List<Material> m_apMaterial = new List<Material>();
+
+
+    void Awake()
+    {
+        m_pCamera = Camera.main;
+        m_pLineRenderer = m_pDrawer.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+            Draw();
+            
+
+        UpdateLine();
+    }
+
+    void Draw()
+    {
+        Vector3 vMousePos = Input.mousePosition;
+        vMousePos.z = m_pCamera.nearClipPlane + 89;
+        Vector3 vMouseWorld = m_pCamera.ScreenToWorldPoint(vMousePos);
+
+        float fDist = Vector3.Distance(m_vLastPos, vMouseWorld);
+        if (fDist <= m_fThreshold)
+            return;
+
+        m_vLastPos = vMouseWorld;
+        if (m_avLinePoints == null)
+            m_avLinePoints = new List<Vector3>();
+        m_avLinePoints.Add(vMouseWorld);
+    }
+
+    public void ChangeMaterial(Material _pMat)
+    {
+        GetComponent<LineRenderer>().material = _pMat;
+        
+        m_avLinePoints = new List<Vector3>();
+    }
+
+    void SavePreviousArray(List<Vector3> _avLinePoints)
+    {
+        List<Vector3> avTempList = _avLinePoints;
+    }
+
+    void UpdateLine()
+    {
+        m_pLineRenderer.SetWidth(m_fStartWidth, m_fEndWidth);
+        m_pLineRenderer.SetVertexCount(m_avLinePoints.Count);
+
+        for (int i = m_iLineCount; i < m_avLinePoints.Count; i++)
+        {
+            m_pLineRenderer.SetPosition(i, m_avLinePoints[i]);
+        }
+        m_iLineCount = m_avLinePoints.Count;
+    }
+}
---
+++ Projet_Unity/Assets/Esther/C#/PaintBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PaintBehavior : MonoBehaviour {
+
+    GameObject m_pDrawManager;
+    Camera m_pCamera;
+
+    // Use this for initialization
+    void Start () {
+
+        m_pDrawManager = GameObject.Find("DrawManager");
+        m_pCamera = Camera.main;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdatePosition();
+	}
+
+    void UpdatePosition()
+    {
+        Vector3 vMousePos = Input.mousePosition;
+        vMousePos.z = m_pCamera.nearClipPlane + 89;
+        Vector3 vMouseWorld = m_pCamera.ScreenToWorldPoint(vMousePos);
+        transform.position = vMouseWorld;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Modifier")
+        {
+            Debug.Log("Hit");
+            Material pMat = other.gameObject.GetComponent<Renderer>().material;
+            if (m_pDrawManager)
+                m_pDrawManager.GetComponent<Paint>().ChangeMaterial(pMat);
+        }
+    }
+}
