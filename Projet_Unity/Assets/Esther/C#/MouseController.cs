987ee890fc36a923311f29e97c33fddf264f3d64
esther5576 <esther.berges@hotmail.com>
2016-10-13 15:03:56 +0200

    Camera + follow laser

--- Projet_Unity/Assets/Esther/C#/MouseController.cs
+++ Projet_Unity/Assets/Esther/C#/MouseController.cs
@@ -6,12 +6,6 @@ public class MouseController : MonoBehaviour
 
 	public GameObject _lacets;
 	public GameObject _pointer;
-
-	GameObject _lacetsInstantiate;
-	GameObject _pointerInstantiate;
-
-	Vector3 offset;
-	Vector3 screenPoint;
 	// Use this for initialization
 	void Start () 
 	{
@@ -21,23 +15,6 @@ public class MouseController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			_pointerInstantiate = GameObject.Instantiate (_pointer, Vector3.zero, Quaternion.identity) as GameObject;
-
-			_lacetsInstantiate = GameObject.Instantiate (_lacets, Input.mousePosition, Quaternion.identity) as GameObject;
-			_lacetsInstantiate.GetComponent<Kvant.WigController> ().target = _pointerInstantiate.transform;
-			_lacetsInstantiate.GetComponent<Kvant.WigController> ().Reset ();
-			_lacetsInstantiate.GetComponent<Kvant.WigController> ().ResetSimulation ();
-		}
-		if (Input.GetMouseButton (0)) 
-		{
-			Debug.Log (">>>>> " + Input.mousePosition);
-		}
-		if (Input.GetMouseButtonUp (0)) 
-		{
-			_lacetsInstantiate = null;
-			_pointerInstantiate = null;
-		}
+		
 	}
 }
