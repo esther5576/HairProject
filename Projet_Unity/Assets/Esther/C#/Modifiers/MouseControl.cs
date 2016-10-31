98b3cd7d21ce2549ce812240e71cff9770cf7ce8
Ben <benjamin.cornaglia@gmail.com>
2016-10-31 16:05:02 +0100

    Modifiers

---
+++ Projet_Unity/Assets/Esther/C#/Modifiers/MouseControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MouseControl : MonoBehaviour {
+
+	public float m_fSpriteDepth = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+	
+		UpdateMousePos ();
+	}
+
+	void UpdateMousePos()
+	{
+		transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + m_fSpriteDepth));
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Modifier") {
+				
+			GetComponent<LineRenderer> ().material = other.gameObject.GetComponent<MeshRenderer> ().material;
+			Debug.Log (other.gameObject.GetComponent<MeshRenderer> ().material.name);
+
+		}
+	}
+
+
+
+}
